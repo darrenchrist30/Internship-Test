71db4ca22cd7635803b859a76ab21e6029c778d8
darrenchrist30 <c14220119@john.petra.ac.id>
2025-06-05 16:41:41 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,14 @@ use App\Http\Controllers\PurchasesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // Arahkan ke halaman input transaksi sebagai default
-    return redirect()->route('purchases.create');
+    return redirect()->route('purchases.create'); //langsung ke halaman create
 });
 
-// Rute untuk halaman input transaksi
+//halaman input
 Route::get('/purchases/create', [PurchasesController::class, 'create'])->name('purchases.create');
 Route::post('/purchases', [PurchasesController::class, 'store'])->name('purchases.store');
 
-// Rute untuk MENAMPILKAN HALAMAN report customer
-// Ini akan memanggil method showCustomerReportPage() di PurchasesController
+//halaman customer
 Route::get('/report/customers', [PurchasesController::class, 'showCustomerReportPage'])->name('report.customers.page');
 
-// Rute untuk AJAX DataTables (MENGAMBIL DATA untuk report customer)
-// Ini akan dipanggil oleh JavaScript DataTables untuk mendapatkan data JSON
-// dan akan memanggil method getCustomerReportData() di PurchasesController
 Route::get('/report/customers/data', [PurchasesController::class, 'getCustomerReportData'])->name('report.customers.data.ajax');
